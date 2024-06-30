a05f8d7ff1d6a26577a8ceeee4e985083e87cadf
phuongbao90 <phuongbao90@gmail.com>
2024-06-30 16:05:16 +0700

    feat: add 96shu.net

---
+++ 96shu.net-npb/src/gen.js
@@ -0,0 +1,36 @@
+function execute(url, page) {
+  if (!page) page = "1";
+  const doc = Http.get("https://96shu.net/" + url + "_" + page + "/").html();
+
+  var next = doc
+    .select("#pagelink")
+    .select("a.next")
+    .attr("href")
+    .split("_")[1]
+    .replace("/", "");
+
+  const el = doc.select(".section-cols ul li");
+
+  const data = [];
+  for (var i = 0; i < el.size(); i++) {
+    var e = el.get(i);
+    var booKId = e.select(".s2 a").first().attr("href").split("/")[4];
+    var fol = booKId.slice(0, -3);
+    data.push({
+      name: e.select(".s2 a").first().text(),
+      link: e.select(".s2 a").first().attr("href"),
+      cover:
+        "https://96shu.net/files/article/image/" +
+        fol +
+        "/" +
+        booKId +
+        "/" +
+        booKId +
+        "s.jpg",
+      description: e.select(".s4").text(),
+      host: "https://96shu.net",
+    });
+  }
+
+  return Response.success(data, next);
+}
